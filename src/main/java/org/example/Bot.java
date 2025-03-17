0bcef4fb6a6486132ecd67a3c07c06a4b9d55035
the_lenoz <you@example.com>
2025-03-17 11:58:06 +0300

    build

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -56,6 +56,10 @@ public class Bot extends TelegramLongPollingBot {
                     }
                 }
 
+                else if (text.startsWith("/start")) {
+                    //Здесь приветствие
+                }
+
             }
         }
     }
